28b018ffcf9e908d9ee0a3e713eeb63bd8d342d8
Tobias Hegge <tobiashegge05@gmail.com>
2024-10-24 19:08:17 +0200

    added and styled searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
 import Card from './components/card/card'
+import Searchbar from './components/searchbar/searchbar'
 import * as pagelists from './components/card/pagelists'
 
 export default function App() {
   return (
     <>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=search" />
+      <div className='Searchbar-container'>
+        <Searchbar />
+      </div>
+
       <div className='Cards'>
         {pagelists.names.map((item, i) => {
           return (<Card name={item} link={pagelists.links[i]}/>)
--- src/components/searchbar/searchbar.tsx
+++ src/components/searchbar/searchbar.tsx
@@ -3,7 +3,16 @@ import './searchbar.css'
 export default function Searchbar() {
     return (
         <>
-            <div>HEI</div>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=search" />
+
+            <div className='Search-bar'>
+                <input type="text" className='Search-input' placeholder='Search on Google...'/>
+                <button className='Search-submit'>
+                    <i className='fa-solid fa-magnifying-glass'>
+                        <span className="Search-icon material-symbols-outlined">search</span>
+                    </i>
+                </button>
+            </div>
         </>
     )
 }
