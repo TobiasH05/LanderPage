9ccb1fa053053cb7f08441b1652fb11189ff4bba
Tobias Hegge <tobiaheg@stud.ntnu.no>
2024-10-24 15:40:10 +0200

    formatted code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import './App.css';
-import Card from './components/card/card';
+import './App.css'
+import Card from './components/card/card'
 import * as pagelists from './components/card/pagelists'
 
-function App() {
+export default function App() {
   return (
     <>
       <div className='Cards'>
-        {pagelists.names.map((item) => {
-          return (<Card name={item} link='https://www.youtube.com/'/>)
+        {pagelists.names.map((item, i) => {
+          return (<Card name={item} link={pagelists.links[i]}/>)
         })}
       </div>
     </>
-  );
+  )
 }
-
-export default App;
--- src/components/card/pagelists.tsx
+++ src/components/card/pagelists.tsx
@@ -2,3 +2,8 @@ export const names: string[] = [
     "YouTube",
     "Netflix"
 ]
+
+export const links: string[] = [
+    "https://www.youtube.com/",
+    "https://www.netflix.com/no/"
+]
