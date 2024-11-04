5f1bf2edd011c9b25017e7667b903c34fd7cdb07
Tobias Hegge <tobiashegge05@gmail.com>
2024-11-04 15:29:19 +0100

    added autofocus to searchbar

--- src/components/searchbar/searchbar.tsx
+++ src/components/searchbar/searchbar.tsx
@@ -6,7 +6,7 @@ export default function Searchbar() {
             <form method='get' action="https://www.google.com/search?">
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=search"/>
                 <div className='Search-bar'>
-                    <input type="text" className='Search-input' placeholder='Search on Google...' name='q'/>
+                    <input type="text" className='Search-input' placeholder='Search on Google...' name='q' autoFocus/>
                     <button className='Search-submit'>
                         <i className='fa-solid fa-magnifying-glass'>
                             <span className="Search-icon material-symbols-outlined">search</span>
